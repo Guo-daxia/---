b3f7ce7bf892fc97656904e94142aaec5e265913
Guo-daxia <57338056+Guo-daxia@users.noreply.github.com>
2019-11-12 17:18:02 +0800

    VueX的基本用法
    vuex的用法，actions和mutations

---
+++ About.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="about">
+        <h1>This is an about page</h1>
+		<p v-for="(m,idx) in homedata" :key='idx' >{{m.name}}</p>
+    </div>
+</template>
+<script>
+import { mapState, mapActions } from "vuex";
+export default {
+    name: "about",
+    computed: {
+		// ...是使用
+		// ...mapState(["homedata"])
+		homedata(){
+			return this.$store.state.m1.homedata
+		}
+    },
+    methods: {
+        ...mapActions(["getdata"])
+    },
+    mounted() {
+        this.getdata();
+    }
+};
+</script>
---
+++ Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="home">
+        <p v-for="(g,idx) in goods" :key='idx' >{{g.title}}</p>
+    </div>
+    <!-- <p v-for="(g,idx) in goods" :key='idx'>{{g.title}}</p> -->
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import { mapState, mapActions, mapMutations } from "vuex";
+export default {
+    name: "home",
+    components: {
+        HelloWorld
+    },
+    computed: {
+       ...mapState(['goods'])
+    },
+    methods:{
+        ...mapMutations(['setHomeData']),
+        ...mapActions(['getHomeData'])
+    },
+    mounted(){
+        this.getHomeData().then(resp=>{
+            console.log('请求数据完成')
+        })
+    }
+};
+</script>
